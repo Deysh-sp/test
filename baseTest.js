6195623a2b319d85b8e7aa34cf69fc364d430ff1
Deysh-sp <84804826+Deysh-sp@users.noreply.github.com>
2021-05-28 07:43:26 -0400

    Add files via upload

---
+++ baseTest.js
@@ -0,0 +1,45 @@
+
+
+
+<script src="https://github.com/Deysh-sp/test/blob/7a0fb04e0271912ce477461503db9b6d0cc18d6a/jquery.js"></script>
+
+
+
+<script>
+
+
+
+
+
+$(document).ready(function(){ 
+
+
+	var browser = JSON.stringify({
+	  isAndroid: /Android/.test(navigator.userAgent),
+	  isCordova: !!window.cordova,
+	  isEdge: /Edge/.test(navigator.userAgent),
+	  isFirefox: /Firefox/.test(navigator.userAgent),
+	  isChrome: /Google Inc/.test(navigator.vendor),
+	  isChromeIOS: /CriOS/.test(navigator.userAgent),
+	  isChromiumBased: !!window.chrome && !/Edge/.test(navigator.userAgent),
+	  isIE: /Trident/.test(navigator.userAgent),
+	  isIOS: /(iPhone|iPad|iPod)/.test(navigator.platform),
+	  isOpera: /OPR/.test(navigator.userAgent),
+	  isSafari: /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent),
+	  isTouchScreen: ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
+	  isWebComponentsSupported: 'registerElement' in document && 'import' in document.createElement('link') && 'content' in document.createElement('template')
+	}, null, '  ');
+	
+	
+	
+	console.log(browser)
+	
+	
+}); //This is the end of the beginning	
+
+
+
+
+
+
+</script>
